cbe424db1cf090df781778b54d48490186e65cb8
lbutler <lukepbutler@gmail.com>
2026-02-06 12:49:39 -0500

    middleware logging

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,20 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
   // Only redirect from the root path based on browser language preference
+  console.log("middleware");
+  console.log(context.url.pathname);
+  console.log(context.preferredLocale);
   if (context.url.pathname === "/") {
     const preferred = context.preferredLocale;
+    console.log("On root path");
     if (preferred === "es") {
+      console.log("Redirecting to /es/");
       return context.redirect("/es/");
+    } else {
+      console.log("Redirecting to /");
+      return context.redirect("/");
     }
   }
+  console.log("Not on root path");
   return next();
 });
